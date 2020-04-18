19c35dd1c97a9e3159f76571b4d7a3b54daeff53
mcruz <mcruzpr86@gmail.com>
2020-04-18 19:30:31 -0400

    basic template made

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+// The specific letters that the user typed.
+let guesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Setting for zero
+let wins = 0;
+let losses = 0;
+let guessesLeft = 9;
+let letterUser = [];
+let eachofLetters = null;
+
+
+// Sets the computerGuess variable equal to a random choice from the computerChoice array.
+var computerGuess = guesses[Math.floor(Math.random() * guesses.length)];
+
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+function farUserGuesses() {
+	document.querySelector("#letter").innerHTML = "Your Guesses so far: " + letterUser.join(' ');
+}
+
+countGuessesLeft();
+
+var restart = function() {
+	guessesLeft = 9;
+	letterUser = [];
+	var computerGuess = guesses[Math.floor(Math.random() * guesses.length)];
+}
+
+// When the user presses a key, it will run the following function..
+document.onkeyup = function(event) {
+	guessesLeft--;
+
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	letterUser.push(userGuess);
+	countGuessesLeft();
+	farUserGuesses();
+
+	if (userGuess === computerGuess){
+		wins++;
+		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		restart();
+	} 
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#lose").innerHTML = "Loses: " + losses;
+		restart();
+	}
+  };
